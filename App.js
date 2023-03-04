e28a89a22f667a44f67adc659eaa93c5cb00f375
IftikharZahid <RaiZahidN@gmail.com>
2023-03-03 23:53:57 -0800

    aaaa

--- App.js
+++ App.js
@@ -1,30 +1,112 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import { Login } from "./src/screens/login";
-import { Register } from "./src/screens/register";
-import { Books } from "./src/screens/books";
-import { BookDetails } from "./src/screens/bookDetails";
-
-export default function App() {
-  
-/*this is business logic area
-all actions are coded in it,
-*/
-
-  const Stack = createNativeStackNavigator();
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  Button,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+export default function myApp() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
+    <View style={{ flex: 3 }}>
+      <View>
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: "bold",
+            marginTop: 50,
+            marginLeft: 5,
+          }}
+        >
+          Sign up
+        </Text>
+        <Image
+          source={require("./assets/logo.png")}
+          style={{ width: 100, height: 100, alignSelf: "center" }}
+        />
+        <Text
+          style={{
+            alignSelf: "center",
+            fontSize: 15,
+            fontWeight: "bold",
+            margin: 10,
+          }}
+        >
+          Welcome ?? To "??ZahidSoftPk??" Add Dev. ??
+        </Text>
+      </View>
+      <ScrollView>
+        <View style={{ flex: 1 }}>
+          <TextInput
+            placeholder="User Name"
+            aria-label="Pak"
+            style={{
+              borderWidth: 1,
+              padding: 10,
+              margin: 10,
+              borderRadius: 15,
+            }}
+          ></TextInput>
+          <TextInput
+            placeholder="Password"
+            style={{
+              borderWidth: 1,
+              padding: 10,
+              margin: 10,
+              borderRadius: 15,
+            }}
+          ></TextInput>
+          <TextInput
+            placeholder="@Email.com"
+            style={{
+              borderWidth: 1,
+              padding: 10,
+              marginLeft: 10,
+              marginRight: 10,
+              marginTop: 10,
+              borderRadius: 15,
+            }}
+          ></TextInput>
+
+          <TouchableOpacity
+            style={{
+              window: "100%",
+              height: 40,
+              alignItems: "flex-end",
+              borderRadius: 15,
+              padding: 10,
+            }}
+          >
+            <Text>Already have an account?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              window: "100%",
+              height: 40,
+              backgroundColor: "red",
+              alignItems: "center",
+              borderRadius: 15,
+              padding: 10,
+              marginLeft: 10,
+              marginRight: 10,
+            }}
+          >
+            <Text style={{ color: "white", fontWeight: "bold" }}>
+              Create new account
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      <Text
+        style={{
+          alignSelf: "center",
+          fontStyle: "italic",
+          margin: 10,
         }}
       >
-        <Stack.Screen name={"login"} component={Login} />
-        <Stack.Screen name={"register"} component={Register} />
-        <Stack.Screen name={"books"} component={Books} />
-        <Stack.Screen name={"bookDetails"} component={BookDetails} />
-      </Stack.Navigator>
-    </NavigationContainer>
+        Coded with ?? by Iftikhar Zahid.
+      </Text>
+    </View>
   );
 }
