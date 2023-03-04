0f93116b2469a428f6bd13be93e8acd6f66613bf
IftikharZahid <RaiZahidN@gmail.com>
2023-03-04 14:33:27 +0500

    business area detection.

--- scr/screens/booksDetail.jsx
+++ scr/screens/booksDetail.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text } from "react-native";
 
 function BookDetails({ navigation, route }) {
+
+  //this is business area to execute any command in app.
   const incomingData = route.params;
 // consol .log is to show output
   console.log("hie");
